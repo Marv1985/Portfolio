500bfad0ce2ef1a0a4274d659edd189a82a201f6
marvin shannon <marvinshannon@hotmail.co.uk>
2025-01-06 11:34:32 +0000

    started making contact page form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,18 @@
+import './Contact.css'
+import { useForm } from "react-hook-form"
+
 const Contact = () => {
-    return(
-        <>
-        <h1>Contact</h1>
-        </>
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (data) => console.log(data)
+  
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("Name", { required: true, maxLength: 100, pattern: /^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/ })} />
+        <input type="email" {...register("Email", { required: true, maxLength: 254 })} />
+        <textarea type="text" {...register("Message",{ required: true, maxLength: 500 })} />
+        
+        <input type="submit" />
+      </form>
     )
 }
 
