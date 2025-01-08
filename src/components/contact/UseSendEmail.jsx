a0f7eedbaa89f8e4ff24577567bff8db1b51b091
marvin shannon <marvinshannon@hotmail.co.uk>
2025-01-08 13:11:16 +0000

    test send

---
+++ src/components/contact/UseSendEmail.jsx
@@ -0,0 +1,50 @@
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const UseSendEmail = () => {
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [showSvg, setShowSvg] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [failed, setFailed] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (resetForm) => {
+    emailjs
+      .sendForm('service_e7x8zas', 'template_bdzz1wa', form.current, {
+        publicKey: 'ejcMDdG0HzSXhDJhO',
+      })
+      .then(
+        () => {
+          setShowSvg(false);
+          setSuccess(true);
+          console.log('Message sent successfully');
+          if (resetForm) resetForm(); // Reset form fields
+        },
+        (error) => {
+          setShowSvg(false);
+          setFailed(true);
+          console.log('Message sending failed', error.text);
+        },
+      );
+
+    setShowOverlay(true);
+    setShowSvg(true);
+
+    setTimeout(() => {
+      setShowOverlay(false);
+      setSuccess(false);
+      setFailed(false);
+    }, 3000);
+  };
+
+  return {
+    sendEmail,
+    showOverlay,
+    showSvg,
+    success,
+    failed,
+    form,
+  };
+};
+
+export default UseSendEmail;
